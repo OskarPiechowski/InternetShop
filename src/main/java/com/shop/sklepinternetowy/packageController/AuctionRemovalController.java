40761383a53cf2e1d9c56640d6d6fc93c002cd20
Oskar <o.piechowski@gmail.com>
2023-03-18 13:36:31 +0100

    shop added removal function

--- src/main/java/com/shop/sklepinternetowy/packageController/AuctionRemovalController.java
+++ src/main/java/com/shop/sklepinternetowy/packageController/AuctionRemovalController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PortController {
-    @GetMapping(path = "add-port")
-    public String getPortForm(){
-        return "port-form";
+public class AuctionRemovalController {
+    @GetMapping(path = "remove-auction")
+    public String getRemoveForm(){
+        return "remove-auction-form";
     }
 }
