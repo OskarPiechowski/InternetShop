af49deff06c872d9bfa7c6ceaadc92172cd3e741
Oskar <o.piechowski@gmail.com>
2023-03-18 12:53:41 +0100

    shop commit

--- src/main/java/com/shop/sklepinternetowy/SklepInternetowyApplication.java
+++ src/main/java/com/shop/sklepinternetowy/SklepInternetowyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SklepInternetowyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SklepInternetowyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SklepInternetowyApplication.class, args);
+    }
 
 }
