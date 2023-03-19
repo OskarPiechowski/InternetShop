94055c1802c281c52aeec3789ea1e183ef66f5a2
Oskar <o.piechowski@gmail.com>
2023-03-19 10:56:01 +0100

    Added AuctionRemoveEquest (name,type,time).

--- src/main/java/com/shop/sklepinternetowy/request/AuctionRemoveRequest.java
+++ src/main/java/com/shop/sklepinternetowy/request/AuctionRemoveRequest.java
@@ -2,4 +2,36 @@ package com.shop.sklepinternetowy.request;
 
 public class AuctionRemoveRequest {
 
+    private String name;
+
+    private String type;
+
+    private int time;
+
+    public AuctionRemoveRequest(String name, String type, int time) {
+        this.name = name;
+        this.type = type;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "AuctionRemoveRequest{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", time=" + time +
+                '}';
+    }
 }
