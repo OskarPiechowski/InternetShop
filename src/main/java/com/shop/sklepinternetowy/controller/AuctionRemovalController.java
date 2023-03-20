51135ad97505c8303867e572f58220e134095c8d
Maksymilian Zalewski <zalewski.maksymilian@gmail.com>
2023-03-20 10:08:26 +0100

    add find-auction and corrects some mess with names etc.

--- src/main/java/com/shop/sklepinternetowy/controller/AuctionRemovalController.java
+++ src/main/java/com/shop/sklepinternetowy/controller/AuctionRemovalController.java
@@ -1,4 +1,4 @@
-package com.shop.sklepinternetowy.packageController;
+package com.shop.sklepinternetowy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
